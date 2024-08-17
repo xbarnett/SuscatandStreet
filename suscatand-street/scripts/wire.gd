9c4beac784ab825894a46811dd84339341011e23
Max Guppy <theonly@mrcat.au>
2024-08-17 16:10:04 +1000

    Add normal vectors to wires

--- suscatand-street/scripts/wire.gd
+++ suscatand-street/scripts/wire.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var start: Vector2
+var startNormal: Vector2 = Vector2.RIGHT
 var end: Vector2
+var endNormal: Vector2 = Vector2.LEFT
+var color = Color.DODGER_BLUE
 
 func _ready() -> void:
 	# Initialize start and end points in _ready
@@ -11,9 +14,15 @@ func _ready() -> void:
 
 func _draw() -> void:
 	# Define the line color and width
-	var line_color = Color.WHITE
-	var line_width = 2
-	draw_line(start, end, line_color, line_width)
+	var line_width = 10
+	var control_distance = start.distance_to(end) / 2
+
+	var curve = Curve2D.new()
+	curve.add_point(start, Vector2(0, 0), startNormal * control_distance)
+	curve.add_point(end, endNormal * control_distance)
+
+	var curve_points = curve.tessellate()
+	draw_polyline(curve.get_baked_points(), color, line_width, true)
 
 func _process(delta: float) -> void:
 	# Update start and end points dynamically
