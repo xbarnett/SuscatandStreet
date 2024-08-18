ebe21a45f3814a534cc45a82a0fa3349b2064cd4
Will Barnett <william@barnett.au>
2024-08-18 10:09:52 +1000

    fix main uwu

--- suscatand-street/scripts/types/model_test.gd
+++ suscatand-street/scripts/types/model_test.gd
@@ -9,21 +9,14 @@ func _ready():
 	
 	var input1 = InputBlock.new(bigNamespace, typeA, true)
 	var input2 = InputBlock.new(bigNamespace, typeB, true)
-	var output1 = OutputBlock.new(bigNamespace, AndType.new(typeA, typeB))
+	var output1 = OutputBlock.new(bigNamespace, typeB)
 	bigNamespace.blocks.append(input1)
 	bigNamespace.blocks.append(input2)
 	bigNamespace.blocks.append(output1)
-
-	var gate = AndMkBlock.new(bigNamespace)
-	input1.connector.make_connection(gate.input2Connector)
-	input2.connector.make_connection(gate.input1Connector)
-	gate.resultConnector.make_connection(output1.connector)
+	
+	input2.connector.make_connection(output1.connector)
 
 	print(bigNamespace.typeCheck())
-	print(gate.input1Connector.value, gate.input2Connector.value, gate.resultConnector.value)
-	print(gate.input1Connector.type.equalTo(typeA),
-		gate.input2Connector.type.equalTo(typeB),
-		gate.resultConnector.type.equalTo(AndType.new(typeA, typeB)))
 
 
 
